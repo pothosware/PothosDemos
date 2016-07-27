e296d4f31b977be70aa65601796ffd473da32feb
Josh Blum <josh@joshknows.com>
2016-07-26 17:27:11 -0700

    custom python type, field typo

--- custom_types/SpikeyData.py
+++ custom_types/SpikeyData.py
@@ -1,7 +1,7 @@
 class SpikeyData:
     def __init__(self, spike=0):
         self._spike = spike
-        self._ouch = "Ouch!"
+        self.ouch = "Ouch!"
 
     def getSpike(self):
         return self._spike
